373ab4a82a142011002113fd5085fd4855703e7e
Mohamed Shafee <mshafee1993@gmail.com>
2021-01-22 14:26:11 -0500

    add routing for delete requests for campuses

--- api/campus_controller.js
+++ api/campus_controller.js
@@ -3,21 +3,45 @@ const campus_model = require('../db_models/campus')
 const router = express.Router();
 
 router.post('/', (request, response, nextMiddleware) => {
-  console.log(request.body.name)
   campus_model.create({
     name: request.body.name,
     info: request.body.info,
     address: request.body.address,
     img: request.body.img
+  }).then(campus => {
+    response.status(200)
+    .json(campus)
   })
 })
+
 
 router.get('/', (request, response, nextMiddleware) => {
   campus_model.findAll()
   .then(campuses => {
     response.status(200)
     .json({
-      message:"Sucesss", campuses
+      message:"Sucesss", campuses 
+    })
+  })
+  .catch(err => {
+    response.status(500)
+    .json(err)
+  })
+})
+
+router.delete('/', (request, response, nextMiddleware) => {
+  console.log(request.body.id)
+  campus_model.findByPk(request.body.id)
+  .then(campus => {
+    if (!campus) {
+      return response.status(404).json({
+        message: "campus not found"
+      })
+    } 
+    campus.destroy();
+    response.status(200)
+    .json({
+      message:"Sucesss"
     })
   })
   .catch(err => {
--- api/index.js
+++ api/index.js
@@ -4,5 +4,6 @@ const router = express.Router()
 
 // if mount path starts with '/campuses', then load the campus_controller module. 
 router.use('/campuses', require('./campus_controller'));
+// router.use('/students', require('./students'));
 
 module.exports = router;
